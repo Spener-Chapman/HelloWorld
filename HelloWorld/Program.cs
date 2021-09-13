45c065aefc68a73d105c6a245a8c71e932676306
Spencer Chapman <90638083+Spener-Chapman@users.noreply.github.com>
2021-09-13 13:57:43 -0300

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!"); // display text
+            Console.WriteLine(""); // makes a space
+            Console.WriteLine("This is my Github test submission."); // more text next line under
+            Console.WriteLine("");
+            Console.WriteLine("Press E to exit..."); 
+            while (Console.ReadKey().Key != ConsoleKey.E); // user MUST press E in order to continue, in this case exit
+        }
+    }
+}
